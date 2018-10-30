b0f0192f367a03107e4c415e6dcc0c0fa9193d83
Mario Bodemann <mario@contentful.com>
2018-10-30 13:43:04 +0100

    make libraries add dependencies to their api

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    java
+    id("java-library")
 }
 
 repositories {
@@ -8,8 +8,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.contentful", "contentful.java", "add~rich-text-SNAPSHOT")
-    implementation("com.google.code.findbugs", "jsr305", "3.0.2")
+    api("com.github.contentful", "contentful.java", "add~rich-text-SNAPSHOT")
+    api("com.google.code.findbugs", "jsr305", "3.0.2")
 
     testImplementation("junit", "junit", "4.12")
     testImplementation("com.google.truth", "truth", "0.42")
--- html/build.gradle.kts
+++ html/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
-    id("java")
+    id("java-library")
 }
-
 
 repositories {
     mavenCentral()
@@ -9,11 +8,14 @@ repositories {
 }
 
 dependencies {
-    implementation(project(":core"))
-
-    implementation("com.github.contentful", "contentful.java", "add~rich-text-SNAPSHOT")
-    implementation("com.google.code.findbugs", "jsr305", "3.0.2")
+    api(project(":core"))
+    api("com.github.contentful", "contentful.java", "add~rich-text-SNAPSHOT")
+    api("com.google.code.findbugs", "jsr305", "3.0.2")
 
     testImplementation("junit", "junit", "4.12")
     testImplementation("com.google.truth", "truth", "0.42")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
