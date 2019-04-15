a71f50bf8740dfbf11c7679f87282f8a38e49f2f
Mario Bodemann <mario@contentful.com>
2019-04-15 11:07:28 +0200

    Update dependencies
    fixes #4

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    api("com.github.contentful", "contentful.java", "${ext["contentful_version"]}")
+    api("com.github.contentful", "contentful.java", "master")
     implementation("com.google.code.findbugs", "jsr305", "3.0.2")
 
     testImplementation("junit", "junit", "4.12")
