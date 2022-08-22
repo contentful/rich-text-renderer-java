b986e3f41f25c3cd2055e1a66eb8d879e13f4af8
Rafal Niski <rafalniski@gmail.com>
2022-08-22 13:47:16 +0200

    Update dependencies to match proper Kotlin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,10 +7,10 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id.startsWith("com.android")) {
-                useModule("com.android.tools.build:gradle:7.0.2")
+                useModule("com.android.tools.build:gradle:7.2.0")
             }
             if (requested.id.id.startsWith("org.jetbrains.kotlin")) {
-                useVersion("1.5.31")
+                useVersion("1.7.10")
             }
         }
     }
